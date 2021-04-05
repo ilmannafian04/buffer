b9e6234932ef0d5fb29da879b145b39074cac8f2
ilmannafian04 <milmannafian04@gmail.com>
2021-04-05 18:14:33 +0700

    added signup functionality

--- buffer-fe/src/lib/Signup.svelte
+++ buffer-fe/src/lib/Signup.svelte
@@ -1,3 +1,32 @@
-<main>
-  <h1>lol</h1>
-</main>
+<script lang="ts">
+  import axios from 'axios';
+
+  import type { SignUpFormData } from '../types/SignUp';
+
+  let formData: SignUpFormData = {
+    username: '',
+    password: '',
+    email: '',
+    displayName: '',
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post('/api/signup', formData)
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  $: console.log(formData);
+</script>
+
+<h1>Sign up</h1>
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" bind:value={formData.username} placeholder="username" />
+  <input type="password" bind:value={formData.password} placeholder="password" />
+  <input type="email" bind:value={formData.email} placeholder="email" />
+  <input type="text" bind:value={formData.displayName} placeholder="displayName" />
+  <button type="submit">Submit</button>
+</form>
---
+++ buffer-fe/src/types/SignUp.ts
@@ -0,0 +1,6 @@
+export type SignUpFormData = {
+  username: string;
+  password: string;
+  email: string;
+  displayName: string;
+}
