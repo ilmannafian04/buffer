0e2f82c4c8768ea453c258e0b642acd45e6c2448
ilmannafian04 <milmannafian04@gmail.com>
2021-04-07 14:04:51 +0700

    added fe routing

--- buffer-fe/src/App.svelte
+++ buffer-fe/src/App.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-  import Signup from './lib/Signup.svelte';
+  import { Router, Route } from 'svelte-routing';
+
+  import Signup from './lib/auth/Signup.svelte';
+  import Signin from './lib/auth/Signin.svelte';
+  import Home from './lib/Home.svelte';
 </script>
 
-<main>
-  <Signup />
-</main>
+<Router url={''}>
+  <Route path="/signup" component={Signup} />
+  <Route path="/signin" component={Signin} />
+  <Route path="/" component={Home} />
+</Router>
 
 <svelte:head>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
