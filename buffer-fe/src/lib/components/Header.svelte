a2c5d3c55c403a25354fb8c52e00a276f22b7c33
ilmannafian04 <milmannafian04@gmail.com>
2021-04-08 01:14:11 +0700

    added simple navs

---
+++ buffer-fe/src/lib/components/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Link, navigate } from 'svelte-routing';
+
+  const navs: { name: string; path: string }[] = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'Sign In',
+      path: '/signin',
+    },
+    {
+      name: 'Sign Up',
+      path: '/signup',
+    },
+  ];
+
+  const changePage = (path: string) => {
+    navigate(path);
+  };
+</script>
+
+<ul>
+  {#each navs as nav (nav.path)}
+    <li>
+      <a on:click|preventDefault={() => changePage(nav.path)} href="/">{nav.name}</a>
+    </li>
+  {/each}
+</ul>
